b5fd5bbe5f106f2c5bb292eacb5c30513bb0ac82
ClaytonJosue <jogamesbr123@gmail.com>
2023-05-20 23:41:58 -0300

    added the encrypt text to website

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,8 +5,20 @@ const text = document.querySelector("#cript_text");
 const criptButton = document.querySelector("#criptBtn");
 const desencriptButton = document.querySelector("#desencriptBtn");
 
+const rightText = document.querySelector("#typedText");
+const rightContent = document.querySelector("#rightContent");
+const rightSection = document.querySelector("#rightSection");
+
 criptButton.addEventListener("click", () => {
-  console.log(encryptText(text.value));
+  if(text.value != "") {
+    rightContent.classList.add("disabled")
+    rightText.innerHTML = encryptText(text.value);
+    rightSection.classList.add("withOverflow");
+  } else {
+    rightContent.classList.remove("disabled")
+    rightSection.classList.remove("withOverflow");
+    rightText.innerHTML = "";
+  }
 });
 
 desencriptButton.addEventListener("click", () => {
